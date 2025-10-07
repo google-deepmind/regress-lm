02d9c4d72f6b87e0c52e8b45849a9abffabcd7d7
Xingyou Song <xingyousong@google.com>
2025-10-07 08:34:46 -0700

    Fix distributed pretraining
    PiperOrigin-RevId: 816222618

--- regress_lm/pytorch/pretraining.py
+++ regress_lm/pytorch/pretraining.py
@@ -15,9 +15,11 @@
 """Pretraining class for RegressLM."""
 
 import collections
+from typing import Callable, Iterator
 from regress_lm import core
 from regress_lm.pytorch import model as pytorch_model
 import torch
+from torch import nn
 from torch import optim
 from torch import utils
 from torch.optim import lr_scheduler
@@ -26,31 +28,55 @@ DistributedSampler = torch.utils.data.distributed.DistributedSampler
 PyTorchModel = pytorch_model.PyTorchModel
 
 
+class DistributedTrainingWrapper(nn.Module):
+  """A wrapper for the PyTorchModel that enables distributed training.
+
+  nn.parallel.DistributedDataParallel only activates when calling forward().
+  """
+
+  def __init__(self, model: PyTorchModel):
+    super().__init__()
+    self.model = model
+
+  def forward(
+      self, batch: dict[str, torch.Tensor]
+  ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
+    return self.model.compute_losses_and_metrics(batch)
+
+
+Parameters = Iterator[nn.Parameter]
+
+
 class Pretrainer:
   """A class to encapsulate the RegressLM pretraining process."""
 
   def __init__(
       self,
       model: PyTorchModel,
-      optimizer: optim.Optimizer,
-      scheduler: lr_scheduler.LRScheduler,
+      optimizer_factory: Callable[[Parameters], optim.Optimizer],
+      scheduler_factory: Callable[[optim.Optimizer], lr_scheduler.LRScheduler],
       train_ds: utils.data.Dataset[core.Example],
       validation_ds: utils.data.Dataset[core.Example],
       batch_size: int,
-      validation_batch_size: int,
-      is_distributed: bool = False,
+      validation_batch_size: int | None = None,
+      distributed_rank: int | None = None,
       num_data_workers: int = 0,
   ):
-    """Initialises the Pretrainer with all hyperparameters."""
-
     self._model = model
-    self._optimizer = optimizer
-    self._scheduler = scheduler
+
+    self._ddp_model = DistributedTrainingWrapper(model)
+    if distributed_rank is not None:
+      self._ddp_model = nn.parallel.DistributedDataParallel(
+          self._ddp_model, device_ids=[distributed_rank]
+      )
+
+    self._optimizer = optimizer_factory(self._ddp_model.parameters())
+    self._scheduler = scheduler_factory(self._optimizer)
     self._global_step = 0
 
     # Create dataloaders for training and validation.
     self._train_sampler = (
-        DistributedSampler(train_ds) if is_distributed else None
+        DistributedSampler(train_ds) if distributed_rank is not None else None
     )
     self._train_dl = utils.data.DataLoader(
         dataset=train_ds,
@@ -63,7 +89,7 @@ class Pretrainer:
     )
     self._val_dl = utils.data.DataLoader(
         dataset=validation_ds,
-        batch_size=validation_batch_size,
+        batch_size=validation_batch_size or batch_size,
         shuffle=False,
         num_workers=num_data_workers,
         collate_fn=self._model.converter.convert_examples,
@@ -71,13 +97,13 @@ class Pretrainer:
 
   def run_validation_epoch(self) -> dict[str, float]:
     """Runs the validation loop and returns metrics."""
-    self._model.eval()
+    self._ddp_model.eval()
     total_loss, total_items = 0.0, 0
     metric_sums: dict[str, float] = collections.defaultdict(float)
 
     with torch.no_grad():
       for val_batch in self._val_dl:
-        losses_p_ex, metrics = self._model.compute_losses_and_metrics(val_batch)
+        losses_p_ex, metrics = self._ddp_model.forward(val_batch)
         bsz = next(iter(val_batch.values())).size(0)
 
         total_loss += losses_p_ex.sum().item()
@@ -93,10 +119,10 @@ class Pretrainer:
 
   def run_train_step(self, batch: dict[str, torch.Tensor]) -> dict[str, float]:
     """Runs train step and returns metrics."""
-    self._model.train()
+    self._ddp_model.train()
     self._optimizer.zero_grad(set_to_none=True)
 
-    losses_per_example, metrics = self._model.compute_losses_and_metrics(batch)
+    losses_per_example, metrics = self._ddp_model.forward(batch)
     loss = losses_per_example.mean()
     loss.backward()
 
@@ -119,3 +145,11 @@ class Pretrainer:
   @property
   def train_dl(self) -> utils.data.DataLoader:
     return self._train_dl
+
+  @property
+  def optimizer(self) -> optim.Optimizer:
+    return self._optimizer
+
+  @property
+  def scheduler(self) -> lr_scheduler.LRScheduler:
+    return self._scheduler
