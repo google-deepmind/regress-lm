Fix casting issue if encoder is diff dtype (e.g. bfloat16) than decoder and default cast T5Gemma-2 as float32. Also add gradient checkpointing as an option to reduce memory (allows 64K lengths). Also fix slight synchronization issue on trainer during validation pass.
PiperOrigin-RevId: 872716836

regress_lm/pytorch/training.py
@@ -160,7 +160,12 @@ class Trainer:
     )
     total_items = torch.tensor(0)
 
-    with torch.no_grad():
+    if self._use_ddp:  # Don't sync forward pass at validation.
+      no_sync_ctx = self._training_wrapper.no_sync()
+    else:
+      no_sync_ctx = contextlib.nullcontext()
+
+    with torch.no_grad(), no_sync_ctx:
       for val_batch in self._val_dl:
         _, metrics = self._training_wrapper.forward(val_batch)
         bsz = next(iter(val_batch.values())).size(0)